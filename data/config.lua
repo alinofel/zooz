c23867a6b66793d6038d714ab6932c2b3fbb7f9c
alinofel <alinofel36@gmail.com>
2016-10-19 08:40:07 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "zooz v1\nAn advanced administration bot based on TG-CLI written in Lua\n\nhttps://github.com/SEEDTEAM/TeleSeed\n\nAdmins\n@iwals [Founder]\n@imandaneshi [Developer]\n@POTUS [Developer]\n@seyedan25 [Manager]\n@aRandomStranger [Admin]\n\nSpecial thanks to\nawkward_potato\nSiyanew\ntopkecleon\nVamptacus\n\nOur channels\n@teleseedch [English]\n@iranseed [persian]\n\nOur website \nhttp://teleseed.seedteam.org/\n",
+  about_text = "zooz v1\nAn advanced administration bot based on TG-CLI written in Lua",
   enabled_plugins = {
   "admin",
 	"ali_nofel",
